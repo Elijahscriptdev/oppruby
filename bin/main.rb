a7c3e779522bd6ca55cb5f298de8827d90f73e8a
elijahscriptdev <elijahobominuru@gmail.com>
2019-11-25 21:26:39 +0100

    Tic Tac Toe user_interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,67 @@
 #!/usr/bin/env ruby
-puts "Hello World!"
+class Tic
+  # attr_reader :board
+  #
+  # def initialize
+  #   @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  #   @total_moves = 9
+  # end
+
+  def welcome
+    puts ' '
+    puts '******************************'
+    puts '*        TIC-TAC-TOE         *'
+    puts '******************************'
+    puts ' CREATED BY ELIJAH & EDWARD '
+    puts ' '
+    puts '......GAME INSTRUCTIONS.......'
+    puts 'Only two(2) players can play this game'
+    puts 'The first player takes X and the second player takes O'
+    puts 'Choose numbers from 1 to 9 to select desired cell'
+    puts 'No duplicate numbers are allowed'
+    puts '...............................'
+
+    # Request for player names and save them to p1 and p2
+    puts 'Player 1 please enter your name: '
+    p1 = gets.chomp
+
+    puts 'Player 2 please enter your name: '
+    p2 = gets.chomp
+
+    puts "Player 1: #{p1}, Token: X"
+    puts "Player 2: #{p2}, Token: O"
+  end
+
+  #Tic Tac Toe Board
+
+  def display_board
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+
+    puts ' ---------- '
+
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+
+    puts ' ---------- '
+
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
+  def get_position
+    puts 'Wich position do you want to take?'
+    position = gets.chomp
+  end
+
+  # def get_current_player
+  #   if @total_moves%2 == 0
+  #     @current_player = player_two
+  #   else
+  #     @current_player = player_one
+  #   end
+  # end
+
+end
+game = Tic.new
+game.welcome
+game.display_board
+game.get_position
